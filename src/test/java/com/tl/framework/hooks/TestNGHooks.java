b9512cfd086d406e51456540e63700fa2cd20d85
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-10-12 09:31:25 +0530

    Change browser to firefox

--- src/test/java/com/tl/framework/hooks/TestNGHooks.java
+++ src/test/java/com/tl/framework/hooks/TestNGHooks.java
@@ -4,12 +4,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.tl.framework.selenium.api.base.SeleniumBase;
+import com.tl.framework.selenium.api.constants.Browsers;
 
 public class TestNGHooks extends SeleniumBase {
 	
 	@BeforeMethod
 	public void beforeMethod() {
-		browserLaunch();		
+		browserLaunch(Browsers.FIREFOX);		
 		loadUrl("https://www.saucedemo.com/");
 	}
 	
